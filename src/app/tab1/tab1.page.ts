57c71a2ab92da8cf30423222b0ee1c48b9fc3fcf
phanuwit13 <big.phanuwit@gmail.com>
2020-11-22 06:04:19 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as firebase from "firebase";
+import { snapshotToArray } from "../../environments/environment";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
+  item = [];
+  connectStatus = ["ไม่มีการเชื่อมต่อ", "เชื่อมต่ออุปกรณ์"];
+  inputText: string = "";
+  ref = firebase.database().ref("/");
+  timeDate = "";
+  temp: number;
 
-  constructor() {}
+  constructor() {
+    this.ref.on("value", resp => {
+      this.item = snapshotToArray(resp);
+    });
+    this.run();
+  }
 
+  addItem(item) {
+    if (item != undefined && item != null) {
+      let newItem = this.ref.push();
+      newItem.set(item);
+      this.inputText = "";
+    }
+  }
+  async delItem(key) {
+    firebase
+      .database()
+      .ref("/" + key)
+      .remove();
+  }
+
+  run() {
+    setInterval(async () => {
+      let t = new Date();
+      this.timeDate =
+        t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds();
+      this.temp = await this.randomMath();
+    }, 1000);
+  }
+  randomMath() {
+    return Math.floor(Math.random() * 3);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,24 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBIFENQ3epjonyFagKeUuDL0v2Yz3DdQSA",
+  authDomain: "pro-solar.firebaseapp.com",
+  databaseURL: "https://pro-solar.firebaseio.com",
+  projectId: "pro-solar",
+  storageBucket: "pro-solar.appspot.com",
+  messagingSenderId: "645656744850",
+  appId: "1:645656744850:web:462afcfb3ad08766ada74b",
+  measurementId: "G-S45V21KDGF"
+  }
 };
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
